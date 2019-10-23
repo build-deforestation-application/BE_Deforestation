c4292243d90274c3d8e29eb4f4f68edfab02f7cd
Thomas Utsey <tom@edgegluer.com>
2019-10-22 20:44:58 -0400

    create object from csv and seed query table

--- routes/queryRoutes.js
+++ routes/queryRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const knex = require("../db/knex");
+const db = require("../helpers/queryDb");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.send("Query");
+router.get("/", async function(req, res, next) {
+  const data = await db.get();
+  res.status(200).json(data);
 });
 
 module.exports = router;
