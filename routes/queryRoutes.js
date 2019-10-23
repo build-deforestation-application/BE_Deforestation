31aae8e2881f3cd1ec0f665a1ab85bdfee3ae268
Thomas Utsey <tom@edgegluer.com>
2019-10-23 17:53:04 -0400

    debug continues

--- routes/queryRoutes.js
+++ routes/queryRoutes.js
@@ -4,12 +4,14 @@ const knex = require("../db/knex");
 const db = require("../helpers/queryDb");
 
 /* GET home page. */
-router.get("/", async function(req, res, next) {
-  const data = await db.get();
+router.get("/", function(req, res, next) {
+  const data = db.get();
   res.status(200).json(data);
 });
-router.get("/second", async function(req, res) {
-  const data = await db.getsecond();
+
+router.get("/second", function(req, res) {
+  const data = db.getsecond();
   res.status(200).json(data);
 });
+
 module.exports = router;
