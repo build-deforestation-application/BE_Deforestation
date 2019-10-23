c8b0a260e186b5bf793f99a5d910ad05169cfbf9
Thomas Utsey <tom@edgegluer.com>
2019-10-23 15:30:53 -0400

    repositioning then function

--- seeds/forest_data.js
+++ seeds/forest_data.js
@@ -129365,13 +129365,16 @@ const arr2 = [
   }
  ]
 
+
+
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('Forest_dataset_1').del()
     .then(function () {
           return knex('Forest_dataset_1').insert(arr)
+          .then(function(){
+            return knex('forest_dataset_2').insert(arr2)
         });
-    .then(function(){
-        return knex('forest_dataset_2').insert(arr2)
+   
     })
 };
